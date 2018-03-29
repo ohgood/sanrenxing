f7316cec84b3acfb8f98a8f358d73135f9d61f27
wukt <wukt@think-land.com>
2018-03-29 13:28:54 +0800

    使用互斥锁防止缓存击穿

--- src/main/java/com/sanrenxing/shop/helper/RedisConnector.java
+++ src/main/java/com/sanrenxing/shop/helper/RedisConnector.java
@@ -6,6 +6,9 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Redis
  * Created on 2017/8/11.
@@ -17,35 +20,58 @@ public class RedisConnector {
 
     private UserMapper userMapper;
 
+    private Jedis jedis = jedisPool.getResource();
+
+    private ExecutorService executorService = Executors.newFixedThreadPool(10);
+
     public RedisConnector(String host, int port, int timeout) {
         JedisPoolConfig poolConfig = new JedisPoolConfig();
         jedisPool = new JedisPool(poolConfig, host, port, timeout);
     }
 
     public void set(String key, String value) {
-        try (Jedis jedis = jedisPool.getResource()) {
-            jedis.set(key, value);
-        }
+        jedis.set(key, value);
     }
 
     /**
-     * 防止缓存击穿
+     * 防止缓存穿透
      * @param key
      * @return
      */
     public String get(String key) {
-        String value;
-        try (Jedis jedis = jedisPool.getResource()) {
-            value = jedis.get(key);
-            if (StringUtils.isBlank(value)) {
-                this.set(key, value);
-                if (userMapper.findByUsername(key) == null) {
-                    jedis.expire(key, 60 * 5);
-                }
-
-            } else {
-                return value;
+        String value = jedis.get(key);
+        if (StringUtils.isBlank(value)) {
+            this.set(key, value);
+            if (userMapper.findByUsername(key) == null) {
+                jedis.expire(key, 60 * 5);
             }
+
+        } else {
+            return value;
+        }
+        return value;
+    }
+
+    /**
+     * “提前”使用互斥锁(mutex key) 防止缓存击穿
+     * @param key
+     * @return
+     */
+    public String getNoBreakdown(String key) {
+        String value = jedis.get(key);
+        long time = jedis.ttl(key);
+        if (time <= System.currentTimeMillis()) {
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    String keyMutex = "mutex:" + key;
+                    if (jedis.setnx(keyMutex, "1") == 1) {
+                        jedis.expire(keyMutex, 3 * 60);
+                        jedis.set(key, userMapper.findByUsername(key).toString());
+                        jedis.del(keyMutex);
+                    }
+                }
+            });
         }
         return value;
     }
