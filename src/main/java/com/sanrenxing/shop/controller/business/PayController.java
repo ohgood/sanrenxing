9816e550f39c18bd8ba816c303061f0460017201
wukt <wukt@think-land.com>
2018-04-10 15:56:54 +0800

    添加手机号码规则验证

--- src/main/java/com/sanrenxing/shop/controller/business/PayController.java
+++ src/main/java/com/sanrenxing/shop/controller/business/PayController.java
@@ -2,11 +2,15 @@ package com.sanrenxing.shop.controller.business;
 
 import com.sanrenxing.shop.rest.SRXResponse;
 import com.sanrenxing.shop.service.OrderService;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 /**
  * Created on 2018/4/10.
@@ -20,13 +24,15 @@ public class PayController {
     private OrderService orderService;
 
     @RequestMapping(value = "/createOrder", method = RequestMethod.POST)
-    public SRXResponse create(@RequestParam("operator") String operator,
-                              @RequestParam("amount") Integer amount,
-                              @RequestParam("sum") Double sum,
-                              @RequestParam("receiver") String receiver,
+    public SRXResponse create(@NotEmpty @RequestParam("operator") String operator,
+                              @NotNull @RequestParam("amount") Integer amount,
+                              @NotNull @RequestParam("sum") Double sum,
+                              @NotEmpty @RequestParam("receiver") String receiver,
+                              @NotEmpty
+                              @Pattern(regexp = "^((19[0-9])|(13[0-9])|(15[^4,\\\\D])|(18[0,5-9]))\\\\d{8}$", message = "请输入正确的手机号！")
                               @RequestParam("iphone") String iphone,
-                              @RequestParam("address") String address,
-                              @RequestParam("goodId") Integer goodId) {
+                              @NotEmpty  @RequestParam("address") String address,
+                              @NotNull @RequestParam("goodId") Integer goodId) {
 
         return orderService.create(operator, amount, sum, receiver, iphone, address, goodId);
     }
